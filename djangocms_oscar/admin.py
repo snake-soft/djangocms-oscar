202620b63d3d82aa102884438359b2ac6e52787f
Snake-Soft <info@snake-soft.com>
2020-10-25 16:49:58 +0100

    Revert "removed old classes"
    This reverts commit 565f23a349053839702ce71e6d99f18f51a3ad84.

--- djangocms_oscar/admin.py
+++ djangocms_oscar/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from cms.admin.placeholderadmin import PlaceholderAdminMixin
-from .models import MyModel
 
-
-class PlaceholderAdmin(PlaceholderAdminMixin, admin.ModelAdmin):
-    pass
-
-admin.site.register(MyModel, PlaceholderAdmin)
+# Register your models here.
--- djangocms_oscar/models.py
+++ djangocms_oscar/models.py
@@ -1,17 +1,10 @@
 from django.db import models
 
-from cms.models import CMSPlugin, fields
+from cms.models import CMSPlugin
 
 
 class FeaturedProduct(CMSPlugin):
     product = models.ForeignKey('catalogue.Product', on_delete=models.CASCADE)
 
-
-def get_slotname(instance):
-    import pdb; pdb.set_trace()  # <---------
-
-
-
-class MyModel(models.Model):
-    # your fields
-    my_placeholder = fields.PlaceholderField('placeholder_name')
+    def __unicode__(self):
+        return unicode(self.product)
